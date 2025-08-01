5664a3246089f84ceb2d8c3e4ce4c911d899497c
OP-ORION <Xanthekid@gmail.com>
2025-07-31 19:08:35 -0500

    designed basic scene structure and player

---
+++ Scripts/entername.gd
@@ -0,0 +1,20 @@
+extends LineEdit
+
+func _process(delta):
+	self.grab_focus()
+
+func _ready():
+	self.text_changed.connect(text_to_upper)
+	self.text_submitted.connect(name_entered)
+
+func text_to_upper(new_text):
+	text = new_text.to_upper()
+	for letter in text:
+		if not letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+			text = text.replace(letter,"")
+	self.caret_column = text.length()
+
+func name_entered(name):
+	get_tree().change_scene_to_file("res://Scenes/root.tscn")
+	
+	
