ffe66cb2ad14a5528e8061bbd2fdc7df6443c49b
OP-ORION <Xanthekid@gmail.com>
2025-08-16 16:20:17 -0500

    add better physics please

--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -1,9 +1,12 @@
 extends RigidBody2D
 
 
-@export var SPEED := 300.0
+@export var Accelleration := 300.0
 @export var ROLL_SPEED := 300.0
 @export var JUMP_VELOCITY := -400.0
+@export var Max_Speed := -400.0
+@export var Normal_Mass := 1
+@export var Roll_Mass := 5
 var reloading = false
 var canJump = false
 
@@ -14,33 +17,39 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	$RayCast2D.force_raycast_update()
-	if Input.is_action_just_pressed("Up") and $RayCast2D.is_colliding() and lock_rotation:
+	if Input.is_action_just_pressed("Up") and lock_rotation:
 		$RollMin.start()
+		mass = Roll_Mass
 		lock_rotation = false
-		apply_force(Vector2(0,JUMP_VELOCITY))
-
+		apply_force(Vector2(0,JUMP_VELOCITY if canJump and $RayCast2D.is_colliding() else 0))
+		$SnailSprite.play("rolling")
+	
+	#handle move
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
-		$SnailSprite.play("crawling")
-		constant_force = Vector2(direction*SPEED * (1 if lock_rotation else 0),0)
-		apply_torque(ROLL_SPEED*direction * (0 if lock_rotation else 1))
-		$SnailSprite.scale.x = sign(direction) if lock_rotation else 1
+		if (lock_rotation):
+			$SnailSprite.play("crawling")
+			constant_force = Vector2(direction*Accelleration,0)
+			$SnailSprite.scale.x = sign(direction)
+		else:
+			apply_torque(ROLL_SPEED*direction )
 	else:
 		constant_force = Vector2(0,0)
-		$SnailSprite.stop()
+		if lock_rotation: $SnailSprite.stop()
 	
 	#slow to a snails pace
-	if (linear_velocity.abs().x > 100) and lock_rotation:
-		linear_velocity.x = 100 * linear_velocity.sign().x
+	if (linear_velocity.abs().x > Max_Speed) and lock_rotation:
+		linear_velocity.x = Max_Speed * linear_velocity.sign().x
 	
-	
+	#handle stop rolling
 	if (linear_velocity.abs().length()<25) and $RollMin.is_stopped() and not direction:
+		mass = Normal_Mass
 		lock_rotation = true
+		$SnailSprite.play("crawling")
 		rotation = 0
 	
 	if position.y > 1000 and not reloading:
 		reloading = true
 		$Camera2D/AnimationPlayer.play("Zoom_Out")
 		await $Camera2D/AnimationPlayer.animation_finished
-		print(get_tree())
 		get_tree().reload_current_scene()
